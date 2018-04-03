395fba5127d851ea6f0d58c8e80daccb610b1780
heowc <heowc1992@gmail.com>
2018-04-04 00:32:00 +0900

    Initial commit

---
+++ server/src/main/java/com/heowc/EukeraServerApplication.java
@@ -0,0 +1,14 @@
+package com.heowc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EukeraServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EukeraServerApplication.class, args);
+    }
+}
